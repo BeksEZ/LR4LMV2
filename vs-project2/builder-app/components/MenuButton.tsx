d8f55ad52c38bf46c9660d8cff11c0356e4d847e
Максим Копилов <10328074@stud.op.edu.ua>
2025-05-27 19:40:28 +0300

    fixed button link error

--- vs-project2/builder-app/components/MenuButton.tsx
+++ vs-project2/builder-app/components/MenuButton.tsx
@@ -1,11 +1,16 @@
+
 import Link from 'next/link';
 
 export const MenuButton = () => {
-  return (
-    <li className="text-center list-none">  {/* Add list-none here */}
-              <Link href="/menu" className="block self-stretch width px-8 py-7 w-full text-1xl font-bold text-white uppercase bg-blue-600 rounded-[30px] hover:bg-blue-700 transition-colors duration-200  max-md:px-5">
-              МЕНЮ
-              </Link>
-            </li>
-  );
+    // The original <li> had "text-center list-none".
+    // "list-none" is no longer needed as it's not an <li>.
+    // "text-center" can be added to the Link's className to center the text "МЕНЮ".
+    return (
+        <Link
+            href="/menu"
+            className="block self-stretch width px-8 py-7 w-full text-1xl font-bold text-white uppercase bg-blue-600 rounded-[30px] hover:bg-blue-700 transition-colors duration-200 max-md:px-5 text-center" // Added text-center
+        >
+            МЕНЮ
+        </Link>
+    );
 };
