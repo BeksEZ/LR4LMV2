edbc18957fcb2913116941bfeca8a3267ad35180
Максим Копилов <10328074@stud.op.edu.ua>
2025-05-27 13:58:17 +0300

    remove about page

--- vs project2/builder-app/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-/*// app/page.tsx or pages/index.tsx (depending on your Next.js setup)
-import Desktop1 from '../../components/about/About';
-
-export default function Home() {
-  return <Desktop1 />;
-}
-
- */
